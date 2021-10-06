f73f6ac59aeb0821d1264ca95fb6fa33ac61bd7c
Olga <example@gmail.com>
2021-10-06 12:34:32 +0300

    add console app

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 "use strict";
 
-const isChecked = false,
-    isClose = false;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(isChecked || isClose);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
 
